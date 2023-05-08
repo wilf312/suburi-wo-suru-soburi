c76188ae549cd2699bdb4648f01a0b2f9c6560c2
OkadaGenya <g.okada.wilf+github@gmail.com>
2023-05-08 23:00:32 +0900

    ui

--- islands/Chat.tsx
+++ islands/Chat.tsx
@@ -1,32 +1,42 @@
-import { useState } from "preact/hooks";
+import { useEffect, useState } from "preact/hooks";
+
+// // キーの検索
+// const result = await kv.get(["key"]);
 
 export default function Chat() {
-  const [list, setList] = useState([
-    {
-      date: new Date(),
-      text: `text`,
-      name: `aaaa`,
-    },
-    {
-      date: new Date(),
-      text: `text`,
-      name: `bbbb`,
-    },
-    {
-      date: new Date(),
-      text: `text`,
-      name: `cccc`,
-    },
-  ]);
+  const [list, setList] = useState([]);
 
   const [text, setText] = useState(``);
-  const [name, setName] = useState(``);
+  const [name, setName] = useState(localStorage.getItem(`name`) || ``);
+  useEffect(() => {
+    fetch("/api/chat", {
+      method: "get",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+      },
+    }).then((res) => res.json()).then((d) => {
+      console.log({ data: d.list });
+
+      setList(d.list.map((d) => {
+        return { text: d.value, name: d.key[1], date: new Date(d.key[2]) };
+      }));
+    }).catch(console.error);
+  }, []);
 
   return (
     <div>
       <ul>
         {list.map((d) => {
-          return <li>{d.name}: {d.text} {d.date.toISOString()}</li>;
+          return (
+            <li
+              style={{
+                fontSize: `18px`,
+              }}
+            >
+              {d.name}: {d.text} {d.date.toISOString()}
+            </li>
+          );
         })}
       </ul>
       <form
@@ -39,29 +49,81 @@ export default function Chat() {
           if (!text || !name) {
             return;
           }
+          const data = {
+            text,
+            date: new Date(),
+            name: name,
+          };
           setList([
-            {
-              text,
-              date: new Date(),
-              name: name,
-            },
+            data,
             ...list,
           ]);
           setText(``);
+
+          fetch("/api/chat", {
+            method: "POST",
+            headers: {
+              "Accept": "application/json",
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ text, name }),
+          }).then((res) => res.json()).then(console.log).catch(console.error);
         }}
       >
         <input
+          max={50}
+          style={{
+            fontSize: `30px`,
+          }}
+          placeholder={`名前`}
           type="text"
           value={name}
-          onInput={(e) => setName(e.currentTarget.value ?? ``)}
+          onInput={(e) => {
+            const _name = e.currentTarget.value ?? ``;
+            setName(_name);
+            localStorage.setItem(`name`, _name);
+          }}
         />
         <input
+          max={50}
+          style={{
+            fontSize: `30px`,
+          }}
+          placeholder={`入力してください`}
           type="text"
           value={text}
           onInput={(e) => setText(e.currentTarget.value ?? ``)}
         />
-        <button type="submit">send</button>
+        <button
+          style={{
+            fontSize: `50px`,
+          }}
+          type="submit"
+        >
+          send
+        </button>
       </form>
+
+      <button
+        style={{
+          border: `none`,
+          background: `none`,
+          color: `white`,
+        }}
+        type="button"
+        onClick={() => {
+          fetch("/api/chat", {
+            method: "delete",
+            headers: {
+              "Accept": "application/json",
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ secret: `test` }),
+          }).then((res) => res.json()).then(console.log).catch(console.error);
+        }}
+      >
+        全てを消す
+      </button>
     </div>
   );
 }
